e7518c99ff791d73560ff26699e07636e004cb67
Inert <inert@gmail.com>
2023-07-13 15:14:42 +0200

    good

--- excel.py
+++ excel.py
@@ -5,6 +5,7 @@ from openpyxl.styles import Font, PatternFill
 from tkcalendar import Calendar, DateEntry
 import tkinter.messagebox as messagebox
 from tkinter import Toplevel
+from openpyxl.styles import Font, PatternFill
 
 
 # Set the file path and name
@@ -40,6 +41,10 @@ for col_num, category in enumerate(categories, start=1):
     cell.font = header_font
     cell.fill = header_fill
 
+    # Increase the row height
+    row_height = 30  # Adjust the height value as desired
+    worksheet.row_dimensions[1].height = row_height
+
 # Define the predefined company options
 company_options = ['Skaitech', '3DSkai']
 
@@ -56,9 +61,8 @@ task_options = ['Development', 'Control', 'Research', 'Testing', 'Other']
 # Dictionary to store the input values
 input_values = {}
 
+
 # Function to handle the selection process for each category
-
-
 def select_option(category, options):
     try:
         value = simpledialog.askstring("Data Input", f"Enter {category}:")
@@ -69,9 +73,8 @@ def select_option(category, options):
     except ValueError as e:
         messagebox.showerror("Error", str(e))
 
+
 # Function to handle the calendar selection
-
-
 def select_date():
     if hasattr(select_date, 'calendar_open') and select_date.calendar_open:
         return
@@ -85,12 +88,12 @@ def select_date():
         top.destroy()
         select_date.calendar_open = False
 
-    top = Tk()
+    top = Toplevel()
     top.title("Select Date")
 
     # Define the position of the calendar within the pop-up window
     calendar_position_row = 0
-    calendar_position_column = 1
+    calendar_position_column = 0
 
     cal = Calendar(top)
     cal.grid(row=calendar_position_row,
@@ -110,27 +113,26 @@ def select_date():
 
     top.mainloop()
 
+
 # Function to handle validation for the Hours field
-
-
 def validate_hours_input(value):
     if value and not value.replace('.', '').isdigit():
         return False
     return True
 
+
 # Function to handle validation for the Notes field
-
-
 def validate_notes_input(value):
     # Add your custom validation logic for the Notes field here
     return True
 
+
 # Function to handle the confirmation button click
-
-
 def confirm_input():
     try:
-        for i, category in enumerate(categories):
+        # Save the data to the Excel file
+        row_data = []
+        for category in categories:
             value = input_values[category].get()
             if category == 'Date' and not value:
                 raise ValueError("Date must be selected.")
@@ -139,9 +141,54 @@ def confirm_input():
             if category == 'Notes':
                 # Retrieve the value from the Text widget
                 value = entry.get("1.0", "end-1c")
-            worksheet.cell(row=2, column=i + 1, value=value)
+            row_data.append(value)
+
+        worksheet.append(row_data)
         workbook.save(file_path)
-        window.destroy()
+
+        # Clear the input fields and reset option menu values
+        for category in categories:
+            input_values[category].set('')
+
+        # Clear the text in the Notes field
+            entry.delete(1.0, "end")
+
+            # Reset option menu values
+            if category == 'Company':
+                option_menu = company_option_menu['menu']
+                option_menu.delete(0, 'end')
+                for option in company_options:
+                    option_menu.add_command(
+                        label=option, command=lambda value=option: input_values['Company'].set(value))
+                input_values['Company'].set(company_options[0])
+
+            elif category == 'Service Line':
+                option_menu = service_line_option_menu['menu']
+                option_menu.delete(0, 'end')
+                for option in service_options:
+                    option_menu.add_command(
+                        label=option, command=lambda value=option: input_values['Service Line'].set(value))
+                input_values['Service Line'].set(service_options[0])
+
+            elif category == 'Type of Service':
+                option_menu = type_of_service_option_menu['menu']
+                option_menu.delete(0, 'end')
+                for option in type_of_service_options:
+                    option_menu.add_command(
+                        label=option, command=lambda value=option: input_values['Type of Service'].set(value))
+                input_values['Type of Service'].set(type_of_service_options[0])
+
+            elif category == 'Task':
+                option_menu = task_option_menu['menu']
+                option_menu.delete(0, 'end')
+                for option in task_options:
+                    option_menu.add_command(
+                        label=option, command=lambda value=option: input_values['Task'].set(value))
+                input_values['Task'].set(task_options[0])
+
+        # Show success message
+        messagebox.showinfo("Success", "Data saved successfully.")
+
     except Exception as e:
         messagebox.showerror("Error", str(e))
 
@@ -151,14 +198,16 @@ window = Tk()
 window.title("Data Input")
 
 # Configure pop-up window dimensions
-window_width = 500
-window_height = 300
+window_width = 600  # Adjust width as desired
+window_height = 400  # Adjust height as desired
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 x_coordinate = (screen_width - window_width) // 2
 y_coordinate = (screen_height - window_height) // 2
 window.geometry(
     f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
+
+# Rest of the code...
 
 
 for i, category in enumerate(categories):
@@ -169,30 +218,34 @@ for i, category in enumerate(categories):
     # Create option menu for categories with predefined options
     if category == 'Company':
         input_values[category] = StringVar(window, company_options[0])
-        option_menu = OptionMenu(
+        company_option_menu = OptionMenu(
             window, input_values[category], *company_options)
-        # Display option menu for Task category
-        option_menu.grid(row=i, column=1)
+        company_option_menu.config(width=15)  # Adjust the width as desired
+        company_option_menu.grid(row=i, column=1)
 
     elif category == 'Service Line':
         input_values[category] = StringVar(window, service_options[0])
-        option_menu = OptionMenu(
+        service_line_option_menu = OptionMenu(
             window, input_values[category], *service_options)
-        # Display option menu for Task category
-        option_menu.grid(row=i, column=1)
+        # Adjust the width as desired
+        service_line_option_menu.config(width=15)
+        service_line_option_menu.grid(row=i, column=1)
 
     elif category == 'Type of Service':
         input_values[category] = StringVar(window, type_of_service_options[0])
-        option_menu = OptionMenu(
+        type_of_service_option_menu = OptionMenu(
             window, input_values[category], *type_of_service_options)
-        # Display option menu for Task category
-        option_menu.grid(row=i, column=1)
+        type_of_service_option_menu.config(
+            width=15)  # Adjust the width as desired
+        type_of_service_option_menu.grid(row=i, column=1)
 
     elif category == 'Task':
         input_values[category] = StringVar(window, task_options[0])
-        option_menu = OptionMenu(window, input_values[category], *task_options)
-        # Display option menu for Task category
-        option_menu.grid(row=i, column=1)
+        task_option_menu = OptionMenu(
+            window, input_values[category], *task_options)
+        task_option_menu.config(width=15)  # Adjust the width as desired
+        task_option_menu.grid(row=i, column=1)
+
     elif category == 'Date':
         input_values[category] = StringVar(window)
         select_date_button = Button(
@@ -210,7 +263,7 @@ for i, category in enumerate(categories):
     elif category == 'Notes':
         input_values[category] = StringVar(window)
         vcmd = (window.register(validate_notes_input), '%P')
-        entry = Text(window, height=4, width=30)
+        entry = Text(window, height=4, width=26)
         # Use sticky='w' to align the entry widget to the left
         entry.grid(row=i, column=1, columnspan=2, sticky='w')
     else:
