e2e56ccfe4fcbded33fed82aa9407d585fe0aad8
inert <inert.etila@gmail.com>
2023-08-07 11:17:40 +0200

    Reverting to 3fde833

--- excel.py
+++ excel.py
@@ -5,20 +5,15 @@ from openpyxl.styles import Font, PatternFill
 from tkcalendar import Calendar, DateEntry
 import tkinter.messagebox as messagebox
 from tkinter import Toplevel
+from openpyxl.styles import Font, PatternFill
 import datetime
-import tkinter as tk
-from tkinter import ttk
-
-# Function to apply header styles to the worksheet
 
 
 def apply_header_styles(worksheet, headers):
-    # Define header styles
     header_font = Font(bold=True, color="ffffff")
     header_fill = PatternFill(start_color="061c43",
                               end_color="061c43", fill_type="solid")
 
-    # Loop through the headers and apply styles to each cell
     for col_num, header in enumerate(headers, start=1):
         cell = worksheet.cell(row=1, column=col_num, value=header)
         cell.font = header_font
@@ -27,8 +22,6 @@ def apply_header_styles(worksheet, headers):
         row_height = 30
         worksheet.row_dimensions[1].height = row_height
 
-# Function to create or get a sheet from the workbook
-
 
 def create_or_get_sheet(workbook, sheet_name):
     try:
@@ -36,14 +29,10 @@ def create_or_get_sheet(workbook, sheet_name):
     except KeyError:
         return workbook.create_sheet(sheet_name)
 
-# Function to create the directory if it doesn't exist for the Excel data file
-
 
 def create_directory_if_not_exists(file_path):
     directory = os.path.dirname(file_path)
     os.makedirs(directory, exist_ok=True)
-
-# Function to load the existing Excel workbook or create a new one if not found
 
 
 def load_or_create_workbook(file_path):
@@ -52,16 +41,12 @@ def load_or_create_workbook(file_path):
     except FileNotFoundError:
         return openpyxl.Workbook()
 
-# Function to apply header styles to the worksheet
-
 
 def apply_header_styles(worksheet, categories):
-    # Define header styles
     header_font = Font(bold=True, color="ffffff")
     header_fill = PatternFill(start_color="061c43",
                               end_color="061c43", fill_type="solid")
 
-    # Loop through the categories and apply styles to each cell
     for col_num, category in enumerate(categories, start=1):
         cell = worksheet.cell(row=1, column=col_num, value=category)
         cell.font = header_font
@@ -69,8 +54,6 @@ def apply_header_styles(worksheet, categories):
 
         row_height = 30
         worksheet.row_dimensions[1].height = row_height
-
-# Function to prompt user for a value from a given list of options
 
 
 def select_option(category, options):
@@ -83,63 +66,41 @@ def select_option(category, options):
     except ValueError as e:
         messagebox.showerror("Error", str(e))
 
-# Function to handle the selection of a date using a calendar widget
-
 
 def select_date():
-    # Check if the 'calendar_open' attribute exists in the function and if it's True
-
     if hasattr(select_date, 'calendar_open') and select_date.calendar_open:
-        # If the calendar window is already open, return without opening another instance
-
         return
 
     select_date.calendar_open = True
 
     def on_date_selected():
-        # Get the selected date from the calendar widget
-
         selected_date = cal.selection_get()
-        # Update the 'Date' entry with the selected date in the format YYYY-MM-DD
-
         input_values['Date'].set(selected_date.strftime("%Y-%m-%d"))
-        # Update the 'date_label' with the selected date
-
         date_label.config(text=input_values['Date'].get())
         top.destroy()
         select_date.calendar_open = False
 
-    # Create and configure the top-level window for the calendar widget
     top = Toplevel()
     top.title("Select Date")
-    top.configure(background='#333333')
 
-    # Configure styles for the calendar and confirm button
-    style = ttk.Style(top)
-    style.configure('Calendar.Treeview', background='black',
-                    foreground='black', fieldbackground='black')
-    style.configure('TButton', foreground='black',
-                    background='#061c43', font=('Helvetica', 9, 'bold'))
+    calendar_position_row = 0
+    calendar_position_column = 0
 
-    # Position the calendar and confirm button
-    calendar_position_row = 1
-    calendar_position_column = 1
     cal = Calendar(top)
     cal.grid(row=calendar_position_row,
              column=calendar_position_column, padx=10, pady=10)
-    confirm_button = ttk.Button(top, text="Confirm", command=on_date_selected)
+
+    confirm_button = Button(top, text="Confirm", command=on_date_selected)
     confirm_button.grid(row=calendar_position_row + 1,
                         column=calendar_position_column, padx=10, pady=10)
 
-    # Function to close the window and reset calendar_open flag
     def close_window():
         top.destroy()
         select_date.calendar_open = False
 
     top.protocol("WM_DELETE_WINDOW", close_window)
-    top.mainloop()
 
-# Function to validate hours input (should be a positive float value)
+    top.mainloop()
 
 
 def validate_hours_input(value):
@@ -147,13 +108,9 @@ def validate_hours_input(value):
         return False
     return True
 
-# Function to validate notes input (any value is valid for this category)
-
 
 def validate_notes_input(value):
     return True
-
-# Function to check if the last entry in the worksheet is on Sunday
 
 
 def is_last_entry_on_sunday(worksheet):
@@ -164,21 +121,17 @@ def is_last_entry_on_sunday(worksheet):
     if last_date_value is not None:
         last_date_value = datetime.datetime.strptime(
             last_date_value, "%Y-%m-%d")
-        # Sunday is represented as 6 in the weekday() function
-        return last_date_value.weekday() == 6
+        # Fidaay is represented as 4 in the weekday() function
+        return last_date_value.weekday() == 4
     return False
-
-# Function to confirm user input and save data to the worksheet
 
 
 def confirm_input():
     global workbook
     try:
         row_data = []
-        # Loop through the categories and get user input for each category
         for category in categories:
             value = input_values[category].get()
-            # Validate and process the input for specific categories
             if category == 'Date' and not value:
                 raise ValueError("Date must be selected.")
             if category == 'Hours' and not validate_hours_input(value):
@@ -188,48 +141,45 @@ def confirm_input():
             row_data.append(value)
 
         # Save data in the original sheet
-        worksheet.append(row_data)
 
         # Create or get the "2023" sheet and save data there too
         sheet_2023 = create_or_get_sheet(workbook, "2023")
-        sheet_2023.append(row_data)
+        today = datetime.date.today()
+        is_monday = (today.weekday() == 0)
+        # Check if the last entry in the original sheet is on Sunday
+        if is_last_entry_on_sunday(worksheet) and is_monday:
+            # Add an empty row before appending new data on Monday
+            sheet_name_to_delete = 'Sheet'
+            delete_sheet("s", sheet_name_to_delete)
+            header_row = ['Date', 'Service Line', 'Type of Service',
+                          'Company', 'Task', 'Hours', 'Notes']
+            create_sheet_with_headers(header_row, row_data)
+            sheet_2023.append([])
 
-        # Save the changes to the workbook
+        worksheet.append(row_data)
+        sheet_2023.append(row_data)
         workbook.save(file_path)
 
-        # Clear the user input fields after successful save
         for category in categories:
             input_values[category].set('')
+
             if category == 'Notes':
                 entry.delete(1.0, "end")
 
-            # Reset the option menus to the default value based on the selected category.
-
             if category == 'Company':
-                # If the category is 'Company', reset the company_option_menu to its default state.
                 reset_option_menu(company_option_menu, company_options)
-
             elif category == 'Service Line':
-                # If the category is 'Service Line', reset the service_line_option_menu to its default state.
                 reset_option_menu(service_line_option_menu, service_options)
-
             elif category == 'Type of Service':
-                # If the category is 'Type of Service', reset the type_of_service_option_menu to its default state.
                 reset_option_menu(type_of_service_option_menu,
                                   type_of_service_options)
-
             elif category == 'Task':
-                # If the category is 'Task', reset the task_option_menu to its default state.
                 reset_option_menu(task_option_menu, task_options)
 
-        # Show a success message to the user
         messagebox.showinfo("Success", "Data saved successfully.")
 
-    # Show an error message if there's an issue with the input or saving
     except Exception as e:
         messagebox.showerror("Error", str(e))
-
-# Function to reset the option menu to the default value
 
 
 def reset_option_menu(option_menu, options):
@@ -241,8 +191,7 @@ def reset_option_menu(option_menu, options):
     input_values[option_menu.category].set(options[0])
 
 
-# File path to the Excel data file where the timesheet information will be saved
-file_path = 'C:\\Users\\User\\Desktop\\excel-data\\Timesheet-managementt.xlsx'
+file_path = '/Users/skaitech/Desktop/excel-data/Timesheet-managementt.xlsx'
 categories = ['Date', 'Service Line', 'Type of Service',
               'Company', 'Task', 'Hours', 'Notes']
 company_options = ['Skaitech', '3DSkai', '-']
@@ -252,15 +201,12 @@ type_of_service_options = ['Software', 'Hardware', 'Other', '-']
 task_options = ['Development', 'Control', 'Research', 'Testing', 'Other', '-']
 input_values = {}
 
-# Create the directory if it doesn't exist and load or create the workbook
+
 create_directory_if_not_exists(file_path)
 workbook = load_or_create_workbook(file_path)
 worksheet = workbook['Sheet']
 
-# Apply header styles to the worksheet
 apply_header_styles(worksheet, categories)
-
-# Function to delete a sheet from the workbook
 
 
 def delete_sheet(wb, sheet_name):
@@ -276,8 +222,6 @@ def delete_sheet(wb, sheet_name):
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
-# Function to create a new sheet with the given headers and row data
-
 
 def create_sheet_with_headers(headers, rd):
     try:
@@ -292,12 +236,9 @@ def create_sheet_with_headers(headers, rd):
         print(f"An error occurred: {str(e)}")
 
 
-# Create the main application window using Tkinter
 window = Tk()
 window.title("Data Input")
-window.configure(background='#333333')
 
-# Set the size and position of the window
 window_width = 600
 window_height = 400
 screen_width = window.winfo_screenwidth()
@@ -307,10 +248,9 @@ y_coordinate = (screen_height - window_height) // 2
 window.geometry(
     f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
 
-# Check the category and create the appropriate input widgets
+
 for i, category in enumerate(categories):
-    label = Label(window, text=category,
-                  background='#333333', foreground='white')
+    label = Label(window, text=category)
     label.grid(row=i, column=0)
 
     if category == 'Company':
@@ -364,9 +304,8 @@ for i, category in enumerate(categories):
         entry = Entry(window, textvariable=input_values[category])
         entry.grid(row=i, column=1, sticky='w')
 
-# Create the 'Confirm' button, which triggers the confirm_input() function when clicked
+
 confirm_button = Button(window, text="Confirm", command=confirm_input)
 confirm_button.grid(row=len(categories), column=0, columnspan=2, pady=10)
 
-# Start the main event loop for the application window
 window.mainloop()
--- main.cpp
+++ main.cpp
@@ -3,29 +3,18 @@
 
 int main() {
     // Replace the path below with the correct path to your Python script
-    std::string pythonScriptPath = "C:\\Users\\User\\Desktop\\excel-data\\excel.py";
-
-    // Check if Python is available and print version
-    int pythonVersionCheck = std::system("python --version");
-    if (pythonVersionCheck != 0) {
-        std::cout << "\033[1;31mPython is not installed or accessible. Aborting.\033[0m" << std::endl;
-        return 1;
-    }
-
-    // Print "made by Inert" along with Python version
-    std::cout << "\033[1;32mPython is installed. Version:\033[0m ";
-    std::system("python --version && echo \033[1;34mMade by Inert - Using Python\033[0m");
+    const char* pythonScriptPath = "/Users/skaitech/Desktop/excel-data/excel.py";
 
     // Build and execute the shell command to run the Python script
-    std::string command = "python ";
+    std::string command = "python3 ";
     command += pythonScriptPath;
 
     int returnCode = std::system(command.c_str());
 
     if (returnCode == 0) {
-        std::cout << "\033[1;32mPython script executed successfully.\033[0m" << std::endl;
+        std::cout << "Python script executed successfully." << std::endl;
     } else {
-        std::cout << "\033[1;31mError executing Python script.\033[0m" << std::endl;
+        std::cout << "Error executing Python script." << std::endl;
     }
 
     return 0;
