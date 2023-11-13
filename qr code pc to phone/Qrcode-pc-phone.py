c7e92a381354d1a383515d38257fe7a36135569c
inerttila <inert.etila@gmail.com>
2023-11-13 13:05:03 +0100

    download excel file func to phone

---
+++ qr code pc to phone/Qrcode-pc-phone.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3.9
+
+import os
+import http.server
+import socketserver
+import qrcode
+
+
+def generate_qr_code_and_start_server(file_path, filename):
+    excel_directory = os.path.dirname(file_path)
+    qr_code_directory = os.path.join(excel_directory, "qr_photo")
+
+    # Create the "qr_code_photo" directory if it doesn't exist
+    os.makedirs(qr_code_directory, exist_ok=True)
+
+    os.chdir(qr_code_directory)
+
+    # Specify the port to run the server on
+    PORT = 8000
+
+    # Start the server
+    Handler = http.server.SimpleHTTPRequestHandler
+    httpd = socketserver.TCPServer(("", PORT), Handler)
+
+    print(f"Serving at http://localhost:{PORT}")
+    print(f"Scan the QR code to download the Excel file on your phone.")
+
+    # Generate QR code with the local server URL
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(f"http://192.168.40.14:{PORT}/{os.path.basename(file_path)}")
+    qr.make(fit=True)
+
+    # Create an image from the QR Code instance
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    # Save the image in the "qr_code_photo" directory
+    img.save(filename)
+
+    httpd.serve_forever()
+
+
+excel_file_path = 'C:\\Users\\User\\Desktop\\Excel-Data-App\\Timesheet-managementt.xlsx'
+generate_qr_code_and_start_server(excel_file_path, "local_qrcode.png")
